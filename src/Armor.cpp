b836fa18e1a1b29a8b7b801dc1124ad3d97e0342
brianslee <brianslee@umail.ucsb.edu>
2017-03-16 18:22:06 -0700

    Getting rid of superfluous classes

--- src/Armor.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Armor.cpp
-//  addfiles
-//
-//  Created by James wang on 2/22/17.
-//  Copyright © 2017 James wang. All rights reserved.
-//
-
-#include "Armor.h"
--- src/Monster.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Monster.cpp
-//  addfiles
-//
-//  Created by James wang on 2/22/17.
-//  Copyright © 2017 James wang. All rights reserved.
-//
-
-#include "Monster.h"
